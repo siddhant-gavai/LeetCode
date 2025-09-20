a38daed8bf0f0b5b79f167535d26d61ba98cf2a9
Siddhant Gavai <siddhantgavai0@gmail.com>
2025-09-20 11:38:37 +0530

    Optimized approch

--- LeetCode11.java
+++ LeetCode11.java
@@ -1,16 +1,41 @@
 public class LeetCode11 {
     // Brute-force approach to find the maximum area of water container
+    // public int maxArea(int[] height) {
+    // int n = height.length;
+    // int maxArea = 0;
+    // for (int i = 0; i < n; i++) {
+    // for (int j = i + 1; j < n; j++) {
+    // int h = Math.min(height[i], height[j]);
+    // int w = j - i;
+    // int area = h * w;
+    // maxArea = Math.max(maxArea, area);
+    // }
+    // }
+    // return maxArea;
+    // }
+
+    // Optimized approach to find the maximum area of water container
+
     public int maxArea(int[] height) {
-        int n = height.length;
         int maxArea = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int h = Math.min(height[i], height[j]);
-                int w = j - i;
-                int area = h * w;
-                maxArea = Math.max(maxArea, area);
+        int n = height.length;
+        int l = 0;
+        int r = n - 1;
+
+        while (l < r) {
+            int h = Math.min(height[l], height[r]);
+            int w = r - l;
+            int area = h * w;
+            maxArea = Math.max(maxArea, area);
+
+            // Move the pointer pointing to the shorter line
+            if (height[l] < height[r]) {
+                l++;
+            } else {
+                r--;
             }
         }
+
         return maxArea;
     }
 
